1dacf678274724845cb44bf7525b38368486bc66
emily <emily@bacheconsulting.com>
2020-11-29 10:30:25 +0100

    kotlin version

---
+++ Kotlin/src/main/kotlin/codingdojo/Sample.kt
@@ -0,0 +1,5 @@
+package codingdojo;
+
+class Sample {
+    
+}
---
+++ Kotlin/src/test/kotlin/codingdojo/SampleTest.kt
@@ -0,0 +1,13 @@
+package codingdojo;
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+
+    @Test
+    fun sample() {    
+        assertEquals("expected", "actual");
+    }
+    
+}
