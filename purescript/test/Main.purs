547dcc1d44f253e53f12117570cad0229daf5938
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-08-12 23:30:10 +0200

    mvp purescript starter

---
+++ purescript/test/Main.purs
@@ -0,0 +1,16 @@
+module Test.Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+main :: Effect Unit
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "a first test" do
+    it "handles 1+1" do
+      (1+1)  `shouldEqual` 2
