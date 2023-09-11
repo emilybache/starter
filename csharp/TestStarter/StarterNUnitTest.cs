2256c28a8d0dd9222b8733b30645117297a4e07f
emily <emily@bacheconsulting.com>
2023-09-11 08:57:39 +0200

    New tests project

--- csharp/TestStarter/StarterNUnitTest.cs
+++ csharp/TestStarter/StarterNUnitTest.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace StarterProject;
+namespace TestStarter;
 
 using NUnit;
 
---
+++ csharp/TestStarter/StarterXUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestStarter;
+
+public class StarterXUnitTest
+{
+    [Fact]
+    public void TestSomething()
+    {
+        Assert.Equal(true, false);
+    }
+}
