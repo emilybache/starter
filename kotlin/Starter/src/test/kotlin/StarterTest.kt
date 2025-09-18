9211d196f2e2925bc67ae7cb1a4491bc5087a12d
emily <emily@bacheconsulting.com>
2025-09-18 19:33:34 +0200

    kotlin starter project

---
+++ kotlin/Starter/src/test/kotlin/StarterTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class StarterTest {
+    @Test
+    fun sample() {
+        assertEquals(true, false)
+    }
+}
