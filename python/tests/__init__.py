b7dc5189ca954207f306b97365ac1d09624d29e5
emily <emily@bacheconsulting.com>
2025-11-03 07:46:08 +0100

    reporters working out of the box

---
+++ python/tests/__init__.py
@@ -0,0 +1,5 @@
+#from approvaltests import set_default_reporter
+#from approvaltests.reporters import GenericDiffReporterFactory
+# Set a different reporter here
+# see https://github.com/approvals/ApprovalTests.Python/blob/main/docs/tutorial/intro-to-reporters.md
+#set_default_reporter(GenericDiffReporterFactory().get_first_working())
--- python/tests/test_something.py
+++ python/tests/test_something.py
@@ -1,6 +1,10 @@
 import pytest
+from approvaltests import verify
 
 from sample import *
 
 def test_something():
     assert True == False
+
+def test_verify():
+    verify("Hello World")
